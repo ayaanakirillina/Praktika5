7ec17dd88555a0a747a0a41c534b86f03972d3a6
ayaanakirillina <62022680+ayaanakirillina@users.noreply.github.com>
2020-03-10 23:47:45 +0900

    Add files via upload

---
+++ Практикум5 №3.cpp
@@ -0,0 +1,32 @@
+//3. Заполните файл input.txt случайными дробными числами (Пример: 2.4 1.5 6.8 1.2 8.9 и т.д).
+
+#include <iostream>
+#include <fstream> //для работы с файлами
+#include <ctime>
+using namespace std;
+void main() 
+{
+	srand(time(NULL));
+	int i,n;
+	cin>>n;
+	float *a = new float[n];
+	float *b = new float[n];
+	ofstream myfile1;
+	myfile1.open("input.txt");
+	for(i=0;i<n;i++)
+	{
+		a[i] = rand() % 101 - 50;
+		b[i] = rand() % 101 - 50;
+		myfile1<<a[i]+0.01*b[i]<<" ";
+	}
+	myfile1.close();
+	delete[] a;
+	system("pause");
+}
+
+
+
+
+
+
+
