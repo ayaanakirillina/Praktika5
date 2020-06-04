741a76bd320e866ff8cbfd61a73719de90144571
ayaanakirillina <62022680+ayaanakirillina@users.noreply.github.com>
2020-06-04 18:23:46 +0900

    Update №6.cpp

--- №6.cpp
+++ №6.cpp
@@ -11,24 +11,25 @@ using namespace std;
 
 string Sum(string s1, string s2)
 {
-	int n1 = s1.length(), n2 = s2.length();
+	int n1 = s1.length(), n2 = s2.length();//находим строку
 	if (n1 > n2)
 	{
-		swap(s1, s2);
+		swap(s1, s2);//делаем более длинную строку второй
 		swap(n1, n2);
 	}
-	string s3;
+	string s3;//строка для записи результата
+	//переведем обе строки
 	reverse(s1.begin(), s1.end());
-	reverse(s2.begin(), s2.end());
+	reverse(s2.begin(), s2.end());//меняем местами первые с последними
 
 	int z = 0;
-	for (int i = 0; i < n1; i++)
+	for (int i = 0; i < n1; i++)//сложим в столбик
 	{
 		int sum = ((s1[i] - '0') + (s2[i] - '0') + z);
 		s3.push_back(sum % 10 + '0');
 		z = sum / 10;
 	}
-	for (int i = n1; i < n2; i++)
+	for (int i = n1; i < n2; i++)//оставшиеся цифры из более длинного 
 	{
 		int sum = ((s2[i] - '0') + z);
 		s3.push_back(sum % 10 + '0');
@@ -36,7 +37,7 @@ string Sum(string s1, string s2)
 	}
 	if (0)
 		s3.push_back(z + '0');//остаток
-	reverse(s3.begin(), s3.end());
+	reverse(s3.begin(), s3.end());//вернем строку результата в запись
 	return s3;
 }
 int main()
